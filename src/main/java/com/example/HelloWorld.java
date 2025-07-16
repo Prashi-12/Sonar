34334265cd8b3c03bbfc1b602861d796981f4e0b
Prashi-12 <prashanth120398@gmail.com>
2025-07-16 13:12:30 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarQube from Jenkins!");
+        System.out.println("Hello, SonarQube from Nikesh!");
     }
 }
