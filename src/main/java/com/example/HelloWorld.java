18309d6aa2309341f6ffaa9fa08c1fc4b839f8b3
Prashi-12 <prashanth120398@gmail.com>
2025-07-16 13:38:42 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarQube from Sahana!");
+        System.out.println("Hello, SonarQube from Prashanth!");
     }
 }
